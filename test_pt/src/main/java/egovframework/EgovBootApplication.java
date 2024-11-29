818dfca1069ca1b3f56cf392bbff47fb5b392359
김성제 <pinkspidar@teraenergy.co.kr>
2024-11-29 16:50:27 +0900

    20241129회사

---
+++ test_pt/src/main/java/egovframework/EgovBootApplication.java
@@ -0,0 +1,19 @@
+package egovframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "egovframework.example")
+//@MapperScan(basePackages = "egovframework.example.dao") // DAO 패키지 경로
+public class EgovBootApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(EgovBootApplication.class, args);
+		System.out.println(context.getBeanDefinitionNames());
+	}
+
+}
